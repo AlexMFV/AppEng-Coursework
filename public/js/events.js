18ef57a155f225a601bb11fecb299501a796eca9
Alex Valente <alexmfv@desktop-cng0rj9.d.port.ac.uk>
2019-11-11 12:38:33 +0000

    Added toolbar buttons implementation

--- public/js/events.js
+++ public/js/events.js
@@ -1,12 +1,32 @@
-/*var el = document.getElementById("editor");
+//window.designMode = "on";
 
-el.onkeydown = function (evt) {
-    evt = evt || window.event;
-    console.log("Key pressed: " + evt.keyCode);
-};*/
+//Enum that defines the buttons
+const types = {"button":"BUTTON", "select":"SELECT"};
 
-/*window.editor.addEventListener("click", change);
+//Runds through all the toolbar children, ie. the buttons to modify the text
+//and adds an eventlistener on click, to execute the required command.
+window.onload = () => {
+  let buttons = document.getElementById('toolbar').children;
 
-function change(e){
-    e.target.setAttribute("style", "background: #0f0;");
-}*/
+  for(let i = 0; i < buttons.length; i++){
+    if(buttons[i].tagName === types.button)
+      buttons[i].addEventListener("click", executeCommand);
+    else if(buttons[i].tagName === types.select)
+      buttons[i].addEventListener("change", executeCommand);
+  }
+};
+
+//Executes the command that's defined on the button (bold, italics)
+function executeCommand(e) {
+  //If there's a value to be determined it splits the string into command and value
+  if(e.target.value.includes(','))
+  {
+      const command = e.target.value.split(',')[0];
+      const value = e.target.value.split(',')[1];
+
+      document.execCommand(command, false, value);
+  }
+  else{
+      document.execCommand(e.target.value, false);
+  }
+}
