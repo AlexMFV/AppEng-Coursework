8768686ab655ae7e1ed1d971f56fccfb6b838752
AlexMFV <alexmfv1@gmail.com>
2019-11-16 18:47:48 +0000

    Paragraphs fully implemented

--- public/js/commands.js
+++ public/js/commands.js
@@ -1,23 +1,46 @@
 function insertParagraph(){
   const selection = document.getSelection();
   const range = selection.getRangeAt(0);
-
-  const endCont = range.endContainer;
+  const endContainer = range.endContainer;
   const parent = range.commonAncestorContainer.parentElement;
 
- //New paragraph to be created
+  //New paragraph to be created
   const elem = document.createElement('div');
   elem.innerHTML = "<br />";
 
-  //If the selection has only text or text encapsulated in any element
-  if(parent.id == "")
-    parent.after(elem);
-  else
-    endCont.after(elem);
+  let index = null; //Index for the new paragraph
+  if(window.editor.innerHTML !== ""){
+    //If the selection's parent is the main editor (then the first line is selected)
+    if(parent.id == "")
+      parent.after(elem);
+    else
+      endContainer.after(elem);
+
+    index = Array.prototype.indexOf.call(window.editor.children, elem);
+  }
+  else{
+    const elem2 = elem.cloneNode(true);
+    window.editor.appendChild(elem);
+    window.editor.appendChild(elem2);
+    index = Array.prototype.indexOf.call(window.editor.children, elem2);
+  }
+
+  if(index != null)
+    setCaretPosition(index, 0);
 
     //TODO:
-    // 1 - Set the cursor on the newly created element
+    // 1 - First paragraph not changing
     // 2 - Handle when the user adds an Enter in the middle of a line
+}
+
+function setCaretPosition(elemIndex, position){
+  const editor = document.getElementById('editor');
+  const range = document.createRange();
+  const sel = window.getSelection();
+  range.setStart(editor.children[elemIndex], position);
+  //range.collapse(true);
+  sel.removeAllRanges();
+  sel.addRange(range);
 }
 
 //----------------------------------------------
@@ -33,8 +56,8 @@ function makeHeading(e){
     const parent = selection.anchorNode.parentElement;
     //If selection's parent is a DIV
     if(parent.nodeName === "DIV"){
-      //If the selection is not the first paragraph
-      if(parent.id == ""){
+      //If the selection is not the first paragraph nor its empty
+      if(parent.id == "" && !parent.classList.contains('container')){
         parent.classList.remove('h1', 'h2', 'h3', 'h4', 'h5', 'h6');
         parent.classList.add('h'+index);
       }
@@ -64,27 +87,27 @@ function boldText(target){
   }
 }
 
-function italicText(target){
-  console.log("Italic On/Off");
-}
-
-function underlineText(target){
-  console.log("Underline On/Off");
-}
-
-function strikeText(target){
-  console.log("Strike On/Off");
-}
-
-function fontText(target, value){
-  console.log("Font Level: " + value);
-}
-
-function encapsulate(text, className){
-  return text = "<span class=" + className + ">" + text + "</span>";
-}
-
-//Function to decapsulate a function
-function decapsulate(text){
-
-}
+//function italicText(target){
+//  console.log("Italic On/Off");
+//}
+//
+//function underlineText(target){
+//  console.log("Underline On/Off");
+//}
+//
+//function strikeText(target){
+//  console.log("Strike On/Off");
+//}
+//
+//function fontText(target, value){
+//  console.log("Font Level: " + value);
+//}
+//
+//function encapsulate(text, className){
+//  return text = "<span class=" + className + ">" + text + "</span>";
+//}
+//
+////Function to decapsulate a function
+//function decapsulate(text){
+//
+//}
