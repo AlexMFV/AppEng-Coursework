0188afb579c8a7993e9f2e874f892e764949079e
AlexMFV <alexmfv1@gmail.com>
2019-11-22 18:42:26 +0000

    Added margin to Paragraph Buttons

--- public/js/classes.js
+++ public/js/classes.js
@@ -2,10 +2,7 @@ class SelectButton{
   constructor(value){
     this.button = document.createElement('button');
     this.button.addEventListener("click", buttonClick);
-    //this.insideElem = document.createElement('i');
-    //this.insideElem.classList.add("fa");
-    //this.insideElem.classList.add("fa-minus");
-    //this.button.appendChild(this.insideElem);
+    this.button.classList.add("paragraphButton");
     this.button.innerText = "-";
     this.button.value = "minus"; //minus or plus
     this.button.setAttribute("contenteditable", false);
