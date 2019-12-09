21d1e87b153f0e43aad9599d6a2c64be8432aaec
AlexMFV <alexmfv1@gmail.com>
2019-12-09 12:00:25 +0000

    Updated SQL Queries

--- database/dummydata.sql
+++ database/dummydata.sql
@@ -2,10 +2,10 @@ insert into Account(usr, pwd) values('Alex', 'a665a45920422f9d417e4867efdc4fb8a0
 insert into Account(usr, pwd) values('Carlitos', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
 insert into Account(usr, pwd) values('Relix', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3');
 
-insert into File(file_name, contents, last_update) values('preset1', 'abc', getdate());
-insert into File(file_name, contents, last_update) values('preset2', 'def', getdate());
-insert into File(file_name, contents, last_update) values('preset3', 'hij', getdate());
-insert into File(file_name, contents, last_update) values('preset4', 'klm', getdate());
+insert into File(file_name, contents, last_update) values('preset1', 'abc', select now());
+insert into File(file_name, contents, last_update) values('preset2', 'def', select now());
+insert into File(file_name, contents, last_update) values('preset3', 'hij', select now());
+insert into File(file_name, contents, last_update) values('preset4', 'klm', select now());
 
 insert into Acc_File(usr_id, file_id) values(1, 1);
 insert into Acc_File(usr_id, file_id) values(2, 2)
