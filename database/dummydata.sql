f29e98c55169c7f0a2192b808d91235e658328b8
AlexMFV <alexmfv1@gmail.com>
2019-12-09 11:52:26 +0000

    Updated SQL, added dummy command

--- database/dummydata.sql
+++ database/dummydata.sql
@@ -7,7 +7,7 @@ insert into File(file_name, contents, last_update) values('preset2', 'def', getd
 insert into File(file_name, contents, last_update) values('preset3', 'hij', getdate());
 insert into File(file_name, contents, last_update) values('preset4', 'klm', getdate());
 
-insert into AccFile(usr_id, file_id) values(1, 1);
-insert into AccFile(usr_id, file_id) values(2, 2)
-insert into AccFile(usr_id, file_id) values(3, 3);
-insert into AccFile(usr_id, file_id) values(1, 4);
+insert into Acc_File(usr_id, file_id) values(1, 1);
+insert into Acc_File(usr_id, file_id) values(2, 2)
+insert into Acc_File(usr_id, file_id) values(3, 3);
+insert into Acc_File(usr_id, file_id) values(1, 4);
